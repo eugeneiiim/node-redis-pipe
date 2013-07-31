a765e3dde8ad34411d03aec982f807925078fdd3
Eugene Marinelli <eugeneiiim@gmail.com>
2013-07-31 15:00:38 -0700

    simple example

---
+++ example.js
@@ -0,0 +1,38 @@
+var async = require('async');
+var spawn = require('child_process').spawn;
+
+var redisPipe = spawn('redis-cli', ['--pipe']);
+
+redisPipe.stdout.setEncoding('utf8');
+
+redisPipe.stdout.pipe(process.stdout);
+redisPipe.stderr.pipe(process.stderr);
+
+var start = Date.now();
+async.times(1000000, function(n, cb) {
+  if (n % 10000 === 0) {
+    console.log(n, '@', (n / (Date.now() - start)) * 1000, 'cmds/s');
+  }
+
+  var cmd = 'SET';
+  var key = 'testo:' + n;
+  var value = n + '';
+
+  var out = [
+    '*3'
+    , '$' + cmd.length
+    , cmd
+    , '$' + key.length
+    , key
+    , '$' + value.length
+    , value
+  ];
+
+  var msg = out.join('\r\n') + '\r\n';
+  //console.log(msg);
+
+  redisPipe.stdin.write(msg);
+  cb();
+}, function() {
+  redisPipe.stdin.end();
+});
